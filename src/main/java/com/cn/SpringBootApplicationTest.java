f799852ead8c3706c48193a69e6f14c1f8116653
liufuqiang <liufuqiang@travelsky.com>
2019-06-26 16:16:44 +0800

    启动外置tomcat

--- src/main/java/com/cn/SpringBootApplicationTest.java
+++ src/main/java/com/cn/SpringBootApplicationTest.java
@@ -2,9 +2,17 @@ package com.cn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootApplicationTest {
+public class SpringBootApplicationTest extends SpringBootServletInitializer {
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootApplicationTest.class);
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApplicationTest.class, args);
     }
