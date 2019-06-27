6ccf612f987529fededda5191b561d36e140ea7f
liufuqiang <liufuqiang@travelsky.com>
2019-06-27 13:32:38 +0800

    1232

--- src/main/java/com/cn/SpringBootApplicationTest.java
+++ src/main/java/com/cn/SpringBootApplicationTest.java
@@ -1,11 +1,15 @@
 package com.cn;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"com.cn.controller","com.cn.service","com.cn.model","com.cn.mapper"})
 public class SpringBootApplicationTest extends SpringBootServletInitializer {
     
     @Override
